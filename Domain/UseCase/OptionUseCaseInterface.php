cdd915f53b909f04c67897778273697efc9f68bf
Matheus Gregorin <math.gregorin@gmail.com>
2025-06-16 17:39:23 -0300

    feat: add methods default on interface

--- Domain/UseCase/OptionUseCaseInterface.php
+++ Domain/UseCase/OptionUseCaseInterface.php
@@ -7,4 +7,6 @@ use Domain\Entities\UserEntity;
 interface OptionUseCaseInterface
 {
     public function receive(UserEntity $user, string $number, ?string $messageId = null);
+    public function returnToMenu(UserEntity $user, string $number, ?string $messageId = null);
+    public function getHitsToDay(UserEntity $user);
 }
